b9886500cfc33d9114361e5005ef334344f0013f
nichoth <nichoth@gmail.com>
2021-02-16 13:31:34 -0800

    hub works

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-https://hub-world.herokuapp.com/
+var signalhub = require('signalhub')
+var hub = signalhub('my-app-name', [
+    'https://hub-world.herokuapp.com/'
+])
+
+hub.subscribe('my-channel')
+    .on('data', function (message) {
+        console.log('new message received', message)
+    })
+
+hub.broadcast('my-channel', { hello: 'world' })
 
