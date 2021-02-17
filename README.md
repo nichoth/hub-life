25ed700d573d45fc714ddac16b166f3f9d6443e6
nichoth <nichoth@gmail.com>
2021-02-17 01:27:47 -0800

    stuff

---
+++ README.md
@@ -0,0 +1,7 @@
+# hub life
+
+Trying [signalhub](https://github.com/mafintosh/signalhub)
+
+So far have just clicked the 'heroku' button in the signalhub readme to make a hub.
+
+
--- webrtc.js
+++ webrtc.js
@@ -4,6 +4,14 @@ var hub = signalhub('my-app-name', [
     'https://hub-world.herokuapp.com/'
 ])
 
+// -------------------------------------
+
+import { render } from 'preact'
+import { html } from 'htm/preact'
+import { useState } from 'preact/hooks'
+
+// --------------------------------------
+
 var swarm = wswarm(hub)
 
 console.log('peers here', swarm.peers)
@@ -13,3 +21,28 @@ swarm.on('peer', function (stream, id) {
     console.log('.peers', swarm.peers)
 })
 
+function App () {
+    var [msgs, setMsgs] = useState([])
+
+    function addMsg (ev) {
+        ev.preventDefault()
+        var msg = ev.target.elements.newMsg.value
+        var newMsgs = msgs.concat([msg])
+        setMsgs(newMsgs)
+    }
+
+    return html`
+        ${msgs.map(msg => {
+            return html`<p class="msg">${msg}</p>`
+        })}
+
+        <form class="msg-input" onSubmit=${addMsg}>
+            <textarea id="newMsg" name="newMsg" cols="44" rows="12"></textarea>
+            <div>
+                <input type="submit" value="new message" />
+            </div>
+        </form>
+    `
+}
+
+render(html`<${App} />`, document.getElementById('content'));
