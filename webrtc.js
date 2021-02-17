14a0e482a9840df09834b5e79280d6b84bd38802
nichoth <nichoth@gmail.com>
2021-02-17 02:36:45 -0800

    seems to work

--- webrtc.js
+++ webrtc.js
@@ -3,6 +3,11 @@ var signalhub = require('signalhub')
 var hub = signalhub('my-app-name', [
     'https://hub-world.herokuapp.com/'
 ])
+// var through = require('through2')
+var observ = require('observ')
+var struct = require('observ-struct')
+var xtend = require('xtend')
+var onend = require('end-of-stream')
 
 // -------------------------------------
 
@@ -16,27 +21,63 @@ var swarm = wswarm(hub)
 
 console.log('peers here', swarm.peers)
 
-swarm.on('peer', function (stream, id) {
-    console.log('CONNECTED to a new peer', id)
-    console.log('.peers', swarm.peers)
+var state = struct({
+    peers: observ({}),
+    msgs: observ([])
 })
 
-function App () {
-    var [msgs, setMsgs] = useState([])
+swarm.on('peer', function (stream, id) {
+    console.log('CONNECTED to a new peer', id)
+    var newPeer = {}
+    newPeer[id] = stream
+    state.peers.set(xtend(state.peers(), newPeer))
 
-    function addMsg (ev) {
+    stream.on('data', function (data) {
+        // var msgs = state.msgs()
+        // state.msgs.set(msgs.concat([data.toString()]))
+        addMsg(data.toString())
+    })
+
+    onend(stream, () => {
+        var peers = state.peers()
+        delete peers[id]
+        state.peers.set(peers)
+    })
+})
+
+function addMsg (msg) {
+    var newMsgs = state.msgs().concat([msg])
+    state.msgs.set(newMsgs)
+}
+
+// need a reference to the stream so we can write to it later
+
+function App () {
+    var [_state, setState] = useState(state())
+    state(function onChange (newState) {
+        console.log('state change', newState)
+        setState(newState)
+    })
+
+    function sendMsg (msg) {
+        Object.keys(_state.peers).forEach(function (peerId) {
+            _state.peers[peerId].write(msg)
+        })
+    }
+
+    function submitMsg (ev) {
         ev.preventDefault()
         var msg = ev.target.elements.newMsg.value
-        var newMsgs = msgs.concat([msg])
-        setMsgs(newMsgs)
+        addMsg(msg)
+        sendMsg(msg)
     }
 
     return html`
-        ${msgs.map(msg => {
+        ${_state.msgs.map(msg => {
             return html`<p class="msg">${msg}</p>`
         })}
 
-        <form class="msg-input" onSubmit=${addMsg}>
+        <form class="msg-input" onSubmit=${submitMsg}>
             <textarea id="newMsg" name="newMsg" cols="44" rows="12"></textarea>
             <div>
                 <input type="submit" value="new message" />
